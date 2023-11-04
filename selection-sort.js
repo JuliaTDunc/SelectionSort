852af241fb54006f670851089ece0fc5f89a86d9
Julia Duncan <juliataylorduncan@gmail.com>
2023-11-04 14:25:19 -0700

    finished

--- selection-sort.js
+++ selection-sort.js
@@ -29,23 +29,29 @@ function selectionSortInPlace(arr) {
   // Set a pointer at zero diving the array into sorted and unsorted halves
   let divider = 0;
   // Repeat while the unsorted half is not empty:
-  while(divider < arr.length-1) {
+  while(divider < arr.length) {
   
     // Find the index of the minimum value in the unsorted half
-    let sub = [...arr];
-    let newSub = sub.splice(divider);
-    let min = Math.min(...newSub);
+    console.log(arr.join(","));
+    let minIndex = divider;
   
     // Save the min value
-
+    for(let i = divider; i < arr.length; i++) {
+      if(arr[i] < arr[minIndex]) {
+        minIndex = i;
+      }
+    }
+    let minValue = arr[minIndex];
     // Shift every unsorted value to the left of the min value to the right by 1
-
+    for(let j = minIndex; j > divider; j--) {
+      arr[j] = arr[j-1];
+    }
     // Put the min value at the divider
-
+    arr[divider] = minValue;
     // Increment the divider and repeat
     divider++;
   }
+  return arr;
 }
-  console.log(selectionSortInPlace([1,5,2,7,3]));
 
 module.exports = [selectionSort, selectionSortInPlace];
