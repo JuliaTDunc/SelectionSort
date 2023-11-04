22649c620a31e5f3c056e0232e42b77647905b06
Julia Duncan <juliataylorduncan@gmail.com>
2023-11-04 13:50:49 -0700

    outofplace

--- selection-sort.js
+++ selection-sort.js
@@ -3,20 +3,23 @@
 function selectionSort(arr) {
 
   // Copy the original array
-
+  let copy =[...arr];
   // Create an array to store the sorted values
-
+  let sorted = [];
   // While the array is not empty...
-
+  while(copy.length > 0) {
     // Do not move this console.log
     console.log(sorted.join(","));
 
     // Find the index of the minimum value in the unsorted half
-
+    let min = Math.min(...copy);
     // Save and remove the value at the min index
-
+    let idx = copy.indexOf(min);
+    copy.splice(idx, 1);
     // Add the min value to the end of the sorted array
-
+    sorted.push(min);
+  }
+  return sorted;
 }
 
 
@@ -24,14 +27,13 @@ function selectionSort(arr) {
 function selectionSortInPlace(arr) {
 
   // Set a pointer at zero diving the array into sorted and unsorted halves
-
+  let divider = 0;
   // Repeat while the unsorted half is not empty:
-
-    // Do not move this console.log
-    console.log(arr.join(","));
-
+  while(divider < arr.length-1) {
+  
     // Find the index of the minimum value in the unsorted half
-
+  
+  
     // Save the min value
 
     // Shift every unsorted value to the left of the min value to the right by 1
@@ -39,7 +41,8 @@ function selectionSortInPlace(arr) {
     // Put the min value at the divider
 
     // Increment the divider and repeat
-
+    divider++;
+  }
 }
 
 
